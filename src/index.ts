f1d4d70600f6cc73dfa4b0074540cd391fa88d58
Kaden-Beck <kadenb12@gmail.com>
2026-02-24 19:44:38 -0700

    Part 1: Adding Type Definitions

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,64 @@
-import "./style.css";
+import './style.css';
 
-const appElement = document.getElementById("app");
-const boardElement = document.getElementById("board");
+const appElement = document.getElementById('app');
+const boardElement = document.getElementById('board');
 const ROW_COUNT = 3;
 const COL_COUNT = 3;
 
-let boardState = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""]
-];
-let currentMove = "X";
+type Cell = "X" | "O" | "";
 
-function createCell(row, col, content = "") {
-  const cell = document.createElement("button");
-  cell.setAttribute("data-row", row.toString());
-  cell.setAttribute("data-col", col.toString());
-  cell.setAttribute("data-content", content);
-  cell.classList.add("cell");
+type TicTacToeBoard = [
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+  [Cell, Cell, Cell],
+];
+
+let boardState: TicTacToeBoard = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
+let currentMove: "X" | "O" = 'X';
+
+function createCell(row: number, col: number, content: Cell = '') {
+  const cell = document.createElement('button');
+  cell.setAttribute('data-row', row.toString());
+  cell.setAttribute('data-col', col.toString());
+  cell.setAttribute('data-content', content);
+  cell.classList.add('cell');
   return cell;
 }
 
 function renderBoard() {
-  if (!appElement) throw new Error("Cannot find app");
-  if (!boardElement) throw new Error("Cannot find board");
-  boardElement.innerHTML = "";
+  if (!appElement) throw new Error('Cannot find app');
+  if (!boardElement) throw new Error('Cannot find board');
+  boardElement.innerHTML = '';
   for (let i = 0; i < ROW_COUNT; i++) {
     for (let j = 0; j < COL_COUNT; j++) {
       boardElement.appendChild(createCell(i, j, boardState[i][j]));
     }
   }
-  const oldMoveElement = document.getElementById("move-element");
+  const oldMoveElement = document.getElementById('move-element');
   if (oldMoveElement) {
     oldMoveElement.remove();
   }
-  const moveElement = document.createElement("p");
-  moveElement.id = "move-element";
+  const moveElement = document.createElement('p');
+  moveElement.id = 'move-element';
   moveElement.innerText = `Next Move: ${currentMove}`;
-  moveElement.classList.add("current-move");
-  appElement.insertBefore(moveElement, document.getElementById("reset"));
+  moveElement.classList.add('current-move');
+  appElement.insertBefore(moveElement, document.getElementById('reset'));
 }
 
 function init() {
-  const resetButton = document.getElementById("reset");
-  if (!resetButton) throw new Error("No Reset button");
-  resetButton.addEventListener("click", () => {
+  const resetButton = document.getElementById('reset');
+  if (!resetButton) throw new Error('No Reset button');
+  resetButton.addEventListener('click', () => {
     boardState = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
     ];
-    currentMove = "X";
+    currentMove = 'X';
     renderBoard();
   });
   renderBoard();
